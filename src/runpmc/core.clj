b616142596ee79566f1241aca11f1a92de8ace1b
Soren Macbeth <soren@dopeness.org>
2015-12-02 11:37:16 -0800

    give things better names

--- src/runpmc/core.clj
+++ src/runpmc/core.clj
@@ -1,5 +1,5 @@
 (ns runpmc.core
-  (:refer-clojure :exclude [rand])
+  (:refer-clojure :exclude [rand merge])
   (:require [clojure.data.int-map :as i])
   (:import [com.carrotsearch.hppc XorShift128P]
            [net.openhft.hashing LongHashFunction]))
@@ -12,7 +12,7 @@
 (defn georand
   "Returns a geometrically distributed random number by determining
   the position of the leftmost 1-bit in a uniformly distributed number `w`."
-  [^long w]
+  [w]
   (let [res (bit-xor (Long/numberOfLeadingZeros (.nextLong rnd)) 0)]
     (if (>= res w)
       (dec w)
@@ -23,68 +23,80 @@
   [m]
   (mod (.nextLong rnd) m))
 
-(defrecord Sketch [l m w int-set n p])
+(defrecord Sketch [bitfield-length vm-rows vm-cols bitfield additions])
 
 (defn create
   "Create a new PMC sketch."
-  ([^long estimated-max]
+  ([estimated-max]
    (create (* estimated-max 32) 256 32))
-  ([l m w]
-   (Sketch. l m w (i/int-set) 0 0.0)))
+  ([bitfield-length vm-rows vm-cols]
+   (Sketch. bitfield-length vm-rows vm-cols (i/int-set) 0)))
 
 (defn find-pos
   "Given item `f` and coordinates `i` and `j` in the virtual matrix
   return the position of `f` in the bit field."
   [sketch ^bytes f i j]
   (let [h (.hashBytes (LongHashFunction/farmNa i j) f)]
-    (mod h (:l sketch))))
+    (mod h (:bitfield-length sketch))))
 
 (defn increment
   "Increment the count for item `f` in `sketch`."
   [sketch ^bytes f]
-  (let [i (rand (:m sketch))
-        j (georand (:w sketch))]
-    (-> (update sketch :n inc)
-        (update :int-set conj (find-pos sketch f i j)))))
+  (let [i (rand (:vm-rows sketch))
+        j (georand (:vm-cols sketch))]
+    (-> (update sketch :additions inc)
+        (update :bitfield conj (find-pos sketch f i j)))))
 
-(defn fp-probability [sketch]
-  (/ (* 1.0 (count (:int-set sketch))) (:l sketch)))
+(defn false-positive-bit-prob
+  "Returns the probability of false positive bits."
+  [sketch]
+  (/ (* 1.0 (count (:bitfield sketch))) (:bitfield-length sketch)))
 
 (defn phi [sketch p]
-  (let [qk (fn [k ^long n ^long p]
+  (let [qk (fn [k n p]
              (reduce * (for [i (range k)]
                          (* (- 1.0 (Math/pow (- 1.0 (Math/pow 2 (- (inc i)))) n)) (- 1.0 p)))))
         e (fn [sketch p]
-            (let [n (:n sketch)]
-              (reduce + (for [x (range (:w sketch))
+            (let [n (:additions sketch)]
+              (reduce + (for [x (range (:vm-cols sketch))
                               :let [k (inc x)]]
                           (* k (- (qk k n p) (qk (inc k) n p)))))))]
-    (/ (Math/pow 2 (e sketch p)) (:n sketch))))
+    (/ (Math/pow 2 (e sketch p)) (:additions sketch))))
 
 (defn zsum [sketch ^bytes f]
   (reduce + (map (fn [i]
-                   (first (keep-indexed #(when-not (contains? (:int-set sketch) (find-pos sketch f i %2)) %1)
-                                        (range (:w sketch)))))
-                 (range (:m sketch)))))
+                   (first (keep-indexed #(when-not (contains? (:bitfield sketch) (find-pos sketch f i %2)) %1)
+                                        (range (:vm-cols sketch)))))
+                 (range (:vm-rows sketch)))))
 
 (defn empty-rows
   "Returns the count of empty rows in the virtual matrix for item `f`."
   [sketch ^bytes f]
-  (reduce + (keep #(when-not (contains? (:int-set sketch) (find-pos sketch f % 0)) 1) (range (:m sketch)))))
+  (reduce + (keep #(when-not (contains? (:bitfield sketch) (find-pos sketch f % 0)) 1) (range (:vm-rows sketch)))))
 
 (defn estimate
   "Return the current estimated count for item `f` in `sketch`."
   [sketch ^bytes f]
-  (let [p (fp-probability sketch)
+  (let [p (false-positive-bit-prob sketch)
         k (empty-rows sketch f)
         kp (/ k (- 1.0 p))
-        e (if (> kp (* 0.3 (:m sketch)))
-            (* -2 (:m sketch) (Math/log (/ kp (:m sketch)))) ;; modified hitcounting
+        e (if (> kp (* 0.3 (:vm-rows sketch)))
+            (* -2 (:vm-rows sketch) (Math/log (/ kp (:vm-rows sketch)))) ;; modified hitcounting
             (let [z (zsum sketch f)]
-              (/ (* (:m sketch) (Math/pow 2 (/ z (:m sketch)))) (phi sketch p))))]
+              (/ (* (:vm-rows sketch) (Math/pow 2 (/ z (:vm-rows sketch)))) (phi sketch p))))]
     (Math/abs e)))
 
 (defn fill-rate
   "Returns the fill rate for `sketch`."
   [sketch]
-  (* (fp-probability sketch) 100))
+  (* (false-positive-bit-prob sketch) 100))
+
+(defn- merge* [sketch1 sketch2]
+  (when (apply not= (map (juxt :bitfield-length :vm-rows :vm-cols) [sketch1 sketch2]))
+    (throw (Exception. "Sketch options must match for merging.")))
+  (assoc sketch1
+         :bitfield (i/union (:bitfield sketch1) (:bitfield sketch2))
+         :additions (+ (:additions sketch1) (:additions sketch2))))
+
+(defn merge [sketch & more]
+  (reduce merge* sketch more))
